7ac7ce3cfc93298bb25a8c1d90161ac862aa1676
Jitesh117 <sahookumarjitesh@gmail.com>
2022-10-13 11:27:37 +0530

    completed mobile UI

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-console.log("Javascript");
+// var url;
+// const shortener = document.querySelector(".shortener");
+// const originalURL = document.querySelector(".shortlinks");
+// const copybutton = document.querySelector("#copybutton");
+// var link;
+// const fetchURL = async () => {
+//   var linkform = document.querySelector("#link");
+//   url = linkform.value;
+//   const res = await fetch(`https://api.shrtco.de/v2/shorten?url=${url}`);
+//    link = await res.json();
+// };
+// function showURL() {
+//   fetchURL();
+//   var result = link.result.short_link;
+//   shortener.style.display = "flex";
+//   originalURL.innerHTML = result;
+//   copybutton.innerHTML = "Copied";
+//   console.log(result);
+// }
+const menu = document.querySelector(".menu");
+var click = 0;
+function hidemenu() {
+  if (click % 2 == 0) menu.style.display = "none";
+  else menu.style.display = "flex";
+  click++;
+}
+
+const linkcard = document.querySelector(".shortener");
+
+function showbutton() {
+  linkcard.style.display = "inherit";
+}
