a5fecc295ad1703978103df5feb323f60f287e04
Jitesh117 <sahookumarjitesh@gmail.com>
2022-10-15 17:50:58 +0530

    finished the desktop view

--- script.js
+++ script.js
@@ -1,32 +1,28 @@
-// var url;
-// const shortener = document.querySelector(".shortener");
-// const originalURL = document.querySelector(".shortlinks");
-// const copybutton = document.querySelector("#copybutton");
-// var link;
-// const fetchURL = async () => {
-//   var linkform = document.querySelector("#link");
-//   url = linkform.value;
-//   const res = await fetch(`https://api.shrtco.de/v2/shorten?url=${url}`);
-//    link = await res.json();
-// };
-// function showURL() {
-//   fetchURL();
-//   var result = link.result.short_link;
-//   shortener.style.display = "flex";
-//   originalURL.innerHTML = result;
-//   copybutton.innerHTML = "Copied";
-//   console.log(result);
-// }
 const menu = document.querySelector(".menu");
-var click = 0;
+var click = 1;
 function hidemenu() {
   if (click % 2 == 0) menu.style.display = "none";
   else menu.style.display = "flex";
   click++;
 }
+var result;
+const fetchURL = async () => {
+  var linkform = document.querySelector("#link");
+  var url = linkform.value;
+  const res = await fetch(`https://api.shrtco.de/v2/shorten?url=${url}`);
+  link = await res.json();
+  var shortlink = link.result.short_link;
+  result = shortlink;
+  const short = document.querySelector(".shortener");
+  const shortlinks = document.querySelector(".shortlinks");
+  shortlinks.innerHTML = shortlink;
+  const copybutton = document.querySelector("#copybutton");
+  copybutton.innerHTML = `Copy`;
+  short.style.display = "inherit";
+};
+function copyURL() {
+  const copybutton = document.querySelector("#copybutton");
+  copybutton.innerHTML = `Copied!`;
 
-const linkcard = document.querySelector(".shortener");
-
-function showbutton() {
-  linkcard.style.display = "inherit";
+  navigator.clipboard.writeText(result);
 }
